3e55dc09ba0d4ac056184d6537cf596629bf1049
nbtd00 <trungkiennguyen01012002@gmail.com>
2024-06-26 23:47:08 +0700

    B63: dựng khung layout mặc định

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import Home from '@/pages/Home';
+import Following from '@/pages/Following';
+import Profile from '@/pages/Profile';
+import Upload from '@/pages/Upload';
+
+const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/following',
+        component: Following,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+    },
+    {
+        path: '/upload',
+        component: Upload,
+    },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
